86dc3b61d73bba696bcadc253470a17c4902a7d6
Nikki Jaramillo <jaramillo.nikki@gmail.com>
2022-04-18 06:07:21 -0700

    created a painting app

---
+++ index.js
@@ -0,0 +1,26 @@
+let selectedColor = 'blue'
+const painting = document.querySelector('.painting')
+
+painting.addEventListener('click', function(e){
+    e.target.style.backgroundColor = selectedColor
+})
+
+let palette = document.querySelector('.palette')
+
+let blue = document.querySelector('#blue')
+let red = document.querySelector('#red')
+let yellow = document.querySelector('#yellow')
+let green = document.querySelector('#green')
+
+blue.addEventListener('click', function(){
+        selectedColor = 'blue'
+})
+red.addEventListener('click', function(){
+    selectedColor = 'red'
+})
+yellow.addEventListener('click', function(){
+    selectedColor = 'yellow'
+})
+green.addEventListener('click', function(){
+    selectedColor = 'green'
+})
